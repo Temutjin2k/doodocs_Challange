b74f5a6a75dff4ee260c0c3f3f3229a5f0e0a8cf
tkoszhan <tamutdzhin2006@mail.ru>
2024-11-17 11:28:48 +0500

    Started using go-mail package for sending emails using smtp

--- internal/service/mail_service.go
+++ internal/service/mail_service.go
@@ -1,10 +1,12 @@
 package service
 
 import (
-	"bytes"
-	"encoding/base64"
+	"errors"
 	"fmt"
-	"net/smtp"
+	"io"
+	"strconv"
+
+	"github.com/go-mail/mail/v2"
 )
 
 type MailServiceImpl interface {
@@ -13,61 +15,57 @@ type MailServiceImpl interface {
 
 type mailService struct {
 	smtpHost string
-	smtpPort string
+	smtpPort int
 	email    string
 	password string
 	smptAddr string
-	auth     smtp.Auth
+	dialer   *mail.Dialer
 }
 
 func NewMailService(smtpHost, smtpPort, email, password string) (*mailService, error) {
-	auth := smtp.PlainAuth("", email, password, smtpHost)
-
-	// test if given parametrs correct
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, email, []string{"kayixa2211@cironex.com"}, []byte("Test request"))
+	port, err := strconv.Atoi(smtpPort)
 	if err != nil {
-		return nil, fmt.Errorf("couldn't create mail service. Error: %v", err)
+		return nil, errors.New("Invalid port. Error: " + err.Error())
 	}
+
+	d := mail.NewDialer(smtpHost, port, email, password)
+	d.StartTLSPolicy = mail.MandatoryStartTLS
 
 	return &mailService{
 		smtpHost: smtpHost,
-		smtpPort: smtpPort,
+		smtpPort: port,
 		email:    email,
 		password: password,
 		smptAddr: smtpHost + ":" + smtpPort,
-		auth:     auth,
+		dialer:   d,
 	}, nil
 }
 
 func (s *mailService) SendFile(mails []string, filename, mimeType string, filedata []byte) error {
-	subject := "Bing!"
-	var msg bytes.Buffer
-	msg.WriteString(fmt.Sprintf("From: %s\r\n", s.email))
-	msg.WriteString("Subject: " + subject + "\r\n")
-	msg.WriteString("MIME-Version: 1.0\r\n")
-	msg.WriteString("Content-Type: multipart/mixed; boundary=\"boundary\"\r\n")
-	msg.WriteString("\r\n--boundary\r\n")
-	msg.WriteString("Content-Type: text/plain; charset=\"utf-8\"\r\n")
-	msg.WriteString("Content-Transfer-Encoding: 7bit\r\n")
-	msg.WriteString("\r\n")
-	msg.WriteString("Hello! Someone send u this file.\r\n")
-	msg.WriteString("\r\n--boundary\r\n")
-	msg.WriteString(fmt.Sprintf("Content-Type: %s; name=\"%s\"\r\n", mimeType, filename))
-	msg.WriteString("Content-Transfer-Encoding: base64\r\n")
-	msg.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=\"%s\"\r\n", filename))
-	msg.WriteString("\r\n")
+	m := mail.NewMessage()
 
-	// Encode file content as base64
-	encodedFile := base64.StdEncoding.EncodeToString(filedata)
-	chunkSize := 76
-	for i := 0; i < len(encodedFile); i += chunkSize {
-		end := i + chunkSize
-		if end > len(encodedFile) {
-			end = len(encodedFile)
-		}
-		msg.WriteString(encodedFile[i:end] + "\r\n")
+	m.SetHeader("From", s.email)
+
+	m.SetHeader("To", mails...)
+	m.SetHeader("Subject", "Bing!")
+	m.SetBody("text/plain", "Hello! Someone sent you this file.")
+
+	// Attaching file
+	m.Attach(filename,
+		mail.SetCopyFunc(func(w io.Writer) error {
+			_, err := w.Write(filedata) // Write the file data directly to the writer
+			return err
+		}),
+		mail.SetHeader(map[string][]string{
+			"Content-Type":        {fmt.Sprintf("%s; name=\"%s\"", mimeType, filename)},
+			"Content-Disposition": {fmt.Sprintf("attachment; filename=\"%s\"", filename)},
+		}),
+	)
+
+	// Send the email
+	if err := s.dialer.DialAndSend(m); err != nil {
+		return fmt.Errorf("failed to send email: %v", err)
 	}
-	msg.WriteString("--boundary--\r\n")
 
-	return smtp.SendMail(s.smptAddr, s.auth, s.email, mails, msg.Bytes())
+	return nil
 }
